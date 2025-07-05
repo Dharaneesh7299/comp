2d911ab9b48928a26da1f93cd1a7a01e92115f37
sivaneshan663 <sneshan555@gmail.com>
2025-07-05 15:00:31 +0530

    studentcontroller

--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,11 @@ const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
 require("dotenv").config();
-const teachroute = require("./routes/teachersRoutes")
+
+const teachroute = require("./routes/teachersRoutes");
+const studentroute = require("./routes/studentRoutes");
 
 const PORT = process.env.PORT || 4000;
-
 
 const app = express();
 
@@ -13,12 +14,12 @@ app.use(express.json());
 app.use(cors());
 app.use(morgan("dev"));
 
-app.get("/",(req,res)=>{
-    res.send("api working");
-})
+app.get("/", (req, res) => {
+  res.send("API working");
+});
 
-app.use("/api/teacher" , teachroute);
-
+app.use("/api/teacher", teachroute);
+app.use("/api/student", studentroute);
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
